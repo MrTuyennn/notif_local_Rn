68e79f1bf93e68ca4fbab286ff3fd54e8d5a2285
MrTuyennn <nguyenngoctuyen188@gmail.com>
2022-02-23 10:31:08 +0700

    start app

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const App = (props: Props) => {
+  return (
+    <View>
+      <Text>Creat app Notif</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
